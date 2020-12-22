03921b0129117963343ed1839786c8ee4287ec93
Mahmoud Kheer Allah <ma6370612@gmail.com>
2020-12-22 19:34:43 +0200

    mahmoud adel

---
+++ app/src/main/java/com/example/finalprojectm/massege.java
@@ -0,0 +1,31 @@
+package com.example.finalprojectm;
+
+public class massege {
+    String sender;
+    String reciver;
+    String message;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReciver() {
+        return reciver;
+    }
+
+    public void setReciver(String reciver) {
+        this.reciver = reciver;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
