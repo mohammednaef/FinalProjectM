465761e22db2e6e8c0e93da6253f907811c62353
Mahmoud Kheer Allah <ma6370612@gmail.com>
2020-12-22 04:09:07 +0200

    mahmoud adel

--- app/src/main/java/com/example/finalprojectm/HomeActivity.java
+++ app/src/main/java/com/example/finalprojectm/HomeActivity.java
@@ -1,14 +1,67 @@
 package com.example.finalprojectm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    AdapterUser adapterUser;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView=findViewById(R.id.Userrecyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager((getApplicationContext())));
+
+        adapterUser=new AdapterUser(getApplicationContext(),ReadUser());
     }
+    public List<User> ReadUser(){
+        final List<User> users=new ArrayList<>();
+
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        reference= FirebaseDatabase.getInstance().getReference("User");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    User user=dataSnapshot.getValue(User.class);
+                    assert  user!=null;
+                    assert  firebaseUser!=null;
+                    if(!user.getId().equals(firebaseUser.getUid())){
+                       users.add(user);
+                    }
+                    recyclerView.setAdapter(adapterUser);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return users;
+    }
+
 }
