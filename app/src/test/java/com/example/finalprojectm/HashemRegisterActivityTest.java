542805fdb225c8bf6e6d8962a4287c321ac6ad07
HashemKaram99 <43606787+HashemKaram99@users.noreply.github.com>
2020-12-28 18:35:15 +0200

    CommitFour

--- app/src/test/java/com/example/finalprojectm/HashemRegisterActivityTest.java
+++ app/src/test/java/com/example/finalprojectm/HashemRegisterActivityTest.java
@@ -26,4 +26,9 @@ public class HashemRegisterActivityTest {
     public void inValidEmailAddress_test3()throws Exception{
         Assert.assertFalse("Email is Not Valid",registerActivity.isInValidEmail("hashem@.com"));
     }
+
+    @Test
+    public void inValidEmailAddress_test4()throws Exception{
+        Assert.assertFalse("Email is Not Valid",registerActivity.isInValidEmail("hashem@gmail."));
+    }
 }
